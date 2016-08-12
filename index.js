7e5b17b468370a74c3f86da63c8eebc334fcdcd6
Dan Cigrang <danielcigrang@me.com>
2016-08-12 18:07:49 -0500

    restructuring the different commands

--- index.js
+++ index.js
@@ -1,17 +1,154 @@
-const Discord = require('discord.js');
-const d20 = require('d20');
-var Auth = require("./auth.json");
-var https = require('https');
-var http = require("http");
-var Spotify = require('spotify-web-api-node');
-var Auth = require('./auth.json');
-var exec = require('child_process').exec;
+try {
+    var Discord = require('discord.js');
+    var d20 = require('d20');
+    var Auth = require("./auth.json");
+    var https = require('https');
+    var http = require("http");
 
-// credentials are optional
-var spotifyApi = new Spotify({
-    clientId: Auth.spotify.clientId,
-    clientSecret: Auth.spotify.clientSecret
-});
+    var Spotify = require('spotify-web-api-node');
+    var spotifyApi = new Spotify({
+        clientId: Auth.spotify.clientId,
+        clientSecret: Auth.spotify.clientSecret
+    });
+
+    var exec = require('child_process').exec;
+} catch (e) {
+    console.log(e.stack);
+    console.log("Run `npm install` and ensure it passes with no errors!");
+    process.exit();
+}
+
+try {
+    var Auth = require('./auth.json');
+} catch (e) {
+    console.log("Please create an auth.json like auth.json.example with at least an email and password.\n" + e.stack);
+    process.exit();
+}
+
+
+var commands = {
+    "ping": {
+        description: "sends `pong!` back for testing",
+        execute: function (bot, msg, action) {
+            //send a message to the channel the ping message was sent in.
+            if (getHelp(action)) {
+                bot.sendMessage(msg, this.description);
+            } else {
+                bot.sendMessage(msg, "pong!");
+            }
+            //alert the console
+            console.log("pong-ed " + msg.author.username);
+        }
+    },
+    "roll": {
+        description: "rolls a fictional 6 sided die",
+        execute: function (bot, msg, action) {
+            //send a message to the channel the ping message was sent in.
+            if (getHelp(action)) {
+                bot.sendMessage(msg, this.description);
+            } else {
+                bot.reply(msg, "Rolled " + d20.roll(6));
+            }
+            //alert the console
+            console.log(msg.author.username + " rolled " + d20.roll(6));
+        }
+    },
+    "topic": {
+        description: "sets the topic of the channel",
+        execute: function (bot, msg, action) {
+            if (getHelp(action)) {
+                bot.sendMessage(msg, this.description);
+            } else {
+                var channel = bot.channels.get('name', 'bot-dev');
+                bot.setChannelTopic(msg.channel, action);
+            }
+        }
+    },
+    "spotify": {
+        description: "return the top song from your search on spotify",
+        execute: function (bot, msg, action) {
+            if (getHelp(action)) {
+                bot.sendMessage(msg, this.description);
+            } else {
+                spotifyApi.searchTracks(action)
+                    .then(function (data) {
+                            bot.sendMessage(msg, data.body.tracks.items[0].external_urls
+                                .spotify);
+                        },
+                        function (err) {
+                            console.error(err);
+                        });
+            }
+        }
+    },
+    "help": {
+        execute: function (bot, msg, action) {
+            bot.sendMessage(msg, "ping, roll, spotify");
+        }
+    },
+    "band": {
+        description: "See an artists upcoming tour dates",
+        execute: function (bot, msg, action) {
+            if (getHelp(action)) {
+
+            } else {
+                var options = {
+                    host: "api.bandsintown.com",
+                    path: "/artists/" + action +
+                        "/events.json?api_version=2.0&app_id=YOUR_APP_ID"
+                };
+                callback = function (response) {
+                    var str = '';
+
+                    //another chunk of data has been recieved, so append it to `str`
+                    response.on('data', function (chunk) {
+                        str += chunk;
+                    });
+
+                    //the whole response has been recieved, so we just print it out here
+                    response.on('end', function () {
+                        var dates = '\n';
+
+                        var strJson = JSON.parse(str);
+                        //console.log(strJson);
+                        for (var i in strJson) {
+                            if (i == 10) {
+                                break;
+                            }
+                            dates += strJson[i].title + " on " + strJson[i].formatted_datetime +
+                                "\n";
+                        }
+                        bot.sendMessage(msg, dates);
+                    });
+                }
+
+                http.request(options, callback).end();
+            }
+        }
+    },
+    "cmd": {
+        description: "dont use this",
+        execute: function (bot, msg, action) {
+            if (getHelp(action)) {
+                bot.sendMessage(msg, this.description);
+            } else {
+                //   var cmd = msg.cleanContent.replace('!cmd ', '');
+                //   console.log(cmd);
+                //   if (cmd === 'pwd' || cmd.indexOf('ls') == 0) {
+                //     exec(cmd, function(error, stdout,
+                //       stderr) {
+                //       bot.sendMessage(msg, stdout);
+                //     });
+                //   }
+            }
+        }
+    }
+};
+
+function getHelp(action) {
+    return action.indexOf('--help') > -1;
+}
+
 
 var bot = new Discord.Client();
 
@@ -31,47 +168,11 @@ bot.on("disconnected", function () {
 
 //when the bot receives a message
 bot.on("message", function (msg, suffix) {
-    //if message begins with "ping"
-    if (msg.content.startsWith("!ping")) {
-        //send a message to the channel the ping message was sent in.
-        bot.sendMessage(msg, "pong!");
-        //bot.sendTTSMessage(msg, "pong");
-
-        //alert the console
-        console.log("pong-ed " + msg.author.username);
-    }
-
-    if (msg.content.startsWith("!roll")) {
-        //send a message to the channel the ping message was sent in.
-        bot.reply(msg, "Rolled " + d20.roll(6));
-        //alert the console
-        //
-        console.log(msg.author.username + " rolled " + d20.roll(6));
-    }
-
-    if (msg.content.startsWith("!topic")) {
-        var channel = bot.channels.get('name', 'general');
-        bot.setChannelTopic(channel, msg.cleanContent.replace('/topic ', ''));
-    }
-
-    if (msg.content.startsWith("!spotify")) {
-        var search = msg.cleanContent.replace('!spotify ', '');
-        spotifyApi.searchTracks(search)
-            .then(function (data) {
-                    bot.sendMessage(msg, data.body.tracks.items[0].external_urls
-                        .spotify);
-                    //bot.sendTTSMessage(msg, search);
-
-                    //console.log('Search by ' + search, data.body.tracks);
-                },
-                function (err) {
-                    console.error(err);
-                });
-
-    }
-
-    if (msg.content.startsWith("!help")) {
-        bot.sendMessage(msg, "ping, roll, spotify");
+    if (msg.content.indexOf("!") == 0) {
+        var stringEnd = (msg.content.indexOf(' ') > -1) ? msg.content.indexOf(' ') : msg.content.length;
+        var cmd = msg.content.substring(1, stringEnd);
+        var action = msg.content.substring(stringEnd + 1, msg.content.length);
+        commands[cmd].execute(bot, msg, action);
     }
 
     if (msg.content.indexOf(bot.user.mention()) == 0 && msg.content.split(" ")[
@@ -90,55 +191,6 @@ bot.on("message", function (msg, suffix) {
         bot.sendMessage(msg.channel, responses[Math.floor(Math.random() *
             responses.length + 1)]);
     }
-
-    if (msg.content.startsWith("!band")) {
-        var search = msg.cleanContent.replace('!band ', '').replace(/\s/g,
-            '%20');
-        console.log(search);
-        var options = {
-            host: "api.bandsintown.com",
-            path: "/artists/" + search +
-                "/events.json?api_version=2.0&app_id=YOUR_APP_ID"
-        };
-        callback = function (response) {
-            var str = '';
-
-            //another chunk of data has been recieved, so append it to `str`
-            response.on('data', function (chunk) {
-                str += chunk;
-            });
-
-            //the whole response has been recieved, so we just print it out here
-            response.on('end', function () {
-                var dates = '\n';
-
-                var strJson = JSON.parse(str);
-                console.log(strJson);
-                for (var i in strJson) {
-                    if (i == 10) {
-                        break;
-                    }
-                    dates += strJson[i].title + " on " + strJson[i].formatted_datetime +
-                        "\n";
-                }
-                bot.sendMessage(msg, dates);
-            });
-        }
-
-        http.request(options, callback).end();
-    }
-
-    //fun stuff but dangerous
-    // if (msg.content.startsWith("!cmd")) {
-    //   var cmd = msg.cleanContent.replace('!cmd ', '');
-    //   console.log(cmd);
-    //   if (cmd === 'pwd' || cmd.indexOf('ls') == 0) {
-    //     exec(cmd, function(error, stdout,
-    //       stderr) {
-    //       bot.sendMessage(msg, stdout);
-    //     });
-    //   }
-    // }
 });
 
 //Log user status changes
